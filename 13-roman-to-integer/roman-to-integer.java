2aea3aed479182b8a4059fc9aa253edd147ae61c
Peer Mohamed Muzammil <pmmuzammilkvp@gmail.com>
2026-03-14 20:37:17 +0530

    Time: 2 ms (99.78%) | Memory: 46.4 MB (74.48%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int romanToInt(String s) {
+
+        int total = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int value = getValue(s.charAt(i));
+
+            if (i < s.length() - 1 && value < getValue(s.charAt(i + 1))) {
+                total -= value;
+            } else {
+                total += value;
+            }
+        }
+
+        return total;
+    }
+
+    public int getValue(char c) {
+
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        if (c == 'M') return 1000;
+
+        return 0;
+    }
+}
