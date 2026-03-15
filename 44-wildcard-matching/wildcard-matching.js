37501ee7e952d5e4318c9cf2aabe4a47210a558c
Peer Mohamed Muzammil <pmmuzammilkvp@gmail.com>
2026-03-15 08:29:00 +0530

    Time: 109 ms (30.13%) | Memory: 90.6 MB (51.32%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.js
@@ -0,0 +1,30 @@
+function isMatch(s, p) {
+    let m = s.length;
+    let n = p.length;
+
+    let dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(false));
+
+    dp[0][0] = true;
+
+    // Handle patterns like "*", "**", "***"
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === "*") {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+
+            if (p[j - 1] === s[i - 1] || p[j - 1] === "?") {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+
+            else if (p[j - 1] === "*") {
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[m][n];
+}
