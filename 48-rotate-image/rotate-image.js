6a773308aeeb1b29cc9c1927ff417d25c5ff191f
Peer Mohamed Muzammil <pmmuzammilkvp@gmail.com>
2026-03-15 08:35:55 +0530

    Time: 1 ms (29.90%) | Memory: 54 MB (72.73%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,15 @@
+function rotate(matrix) {
+    let n = matrix.length;
+
+    // Step 1: Transpose the matrix
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+}
